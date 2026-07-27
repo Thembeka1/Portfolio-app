461a1894894be211194d2cd4320865942b66d351
Thembeka <ttheh98@gmail.com>
2026-07-27 13:47:28 +0200

    new changes

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -38,97 +38,49 @@ export class ContactComponent {
 
   showError = false;
 
-
-
   sendEmail() {
 
-
-    this.showSuccess = false;
-
-    this.showError = false;
-
-
     this.isSending = true;
-
-
-
-    this.emailService.sendEmail(this.emailData)
-      .subscribe({
-
-        next: (response) => {
-
-
-          console.log(response);
-
-
-          this.showSuccess = true;
-
-          this.showError = false;
-
-          this.isSending = false;
-
-
-          console.log("SHOW SUCCESS:", this.showSuccess);
-
-
-
-          // Clear form
-
-          this.emailData = {
-
-            name: '',
-
-            email: '',
-
-            subject: '',
-
-            message: ''
-
-          };
-
-
-
-          // Hide success popup after 3 seconds
-
-          setTimeout(() => {
-
-            this.showSuccess = false;
-
-          }, 3000);
-
-
-        },
-
-
-
-        error: (error) => {
-
-
-          console.error(error);
-
-
-          this.showError = true;
-
+    this.showSuccess = false;
+    this.showError = false;
+  
+    this.emailService.sendEmail(this.emailData).subscribe({
+  
+      next: (response) => {
+  
+        console.log("Success", response);
+  
+        this.isSending = false;
+        this.showSuccess = true;
+  
+        this.emailData = {
+          name: '',
+          email: '',
+          subject: '',
+          message: ''
+        };
+  
+        setTimeout(() => {
           this.showSuccess = false;
-
-          this.isSending = false;
-
-
-
-          setTimeout(() => {
-
-            this.showError = false;
-
-          }, 3000);
-
-
-        }
-
-
-      });
-
-
+        }, 5000);
+  
+      },
+  
+      error: (error) => {
+  
+        console.log(error);
+  
+        this.isSending = false;
+        this.showError = true;
+  
+        setTimeout(() => {
+          this.showError = false;
+        }, 5000);
+  
+      }
+  
+    });
+  
   }
-
-
-}
+  
+  }
